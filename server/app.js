14c6f7dd58fe5ab665977c7214739b622b139210
James C Russell <james@101ideas.cz>
2014-02-27 12:26:40 +0100

    We don't need views.

--- server/app.js
+++ server/app.js
@@ -1,20 +1,18 @@
+#!/usr/bin/env node
 
 /**
  * Module dependencies.
  */
 
 var express = require('express');
-var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = express();
 
-// all environments
+// All environments.
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -25,14 +23,14 @@ app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+// Development.
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+// Routes.
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,10 @@
  * GET users listing.
  */
 
+// exports.index = function(req, res){
+//   res.render('index', { title: 'Express' });
+// };
+
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
