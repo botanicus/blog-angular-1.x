05b2456018b32c4bf74cf8c17264b1744c8c189a
James C Russell <james@101ideas.cz>
2014-03-11 23:42:24 +0100

    Allow CORS, run on port 5000, add /posts.json and /posts/:slug.json routes.

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@
  */
 
 var express = require('express');
+var cors = require('cors');
+var post = require('./routes/post');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
@@ -12,7 +14,7 @@ var path = require('path');
 var app = express();
 
 // All environments.
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -20,6 +22,7 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+app.use(cors());
 app.use(app.router);
 
 // Development.
@@ -28,6 +31,8 @@ if ('development' == app.get('env')) {
 }
 
 // Routes.
+app.get('/posts.json', post.list);
+app.get('/posts/:slug.json', post.get);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function () {
