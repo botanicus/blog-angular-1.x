e3839c90479f7e9260b2574b3c4bb407e52989fb
James C Russell <james@101ideas.cz>
2014-03-16 13:03:40 +0100

    Proper URL when using local Nginx server for development.

--- client/services.js
+++ client/services.js
@@ -3,6 +3,8 @@ var services = angular.module('services', ['ngResource']);
 var apiServerPath = function (path) {
   if (window.location.host.match(/localhost/)) {
     return 'http://localhost:5000' + path;
+  } else if (window.location.host == 'blog.101ideas.dev') {
+    return 'http://api.101ideas.dev' + path;
   } else {
     return 'http://api.101ideas.cz' + path;
   }
